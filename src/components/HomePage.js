dcf00da51d70eba3275cc5621d675f5d338dcc25
Jack Schaffer <jack@Jacks-MacBook-Pro.local>
2020-05-14 16:48:22 -0400

    adding public files back from create-react-app env initialization

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Courses Adminstration</h1>
+      <p>React, Flux, and React Router for ultra-responsive web apps.</p>
+    </div>
+  );
+}
+
+export default HomePage;
