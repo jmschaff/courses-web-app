0d001a827460cfd1cdd5c17821615c1b8968f1c3
Jack Schaffer <jack@Jacks-MacBook-Pro.local>
2020-05-15 14:18:23 -0400

    added client side validation for course form

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TextInput from "./common/TextInput";
+import PropTypes from "prop-types";
 
 function CourseForm(props) {
   return (
@@ -10,6 +11,7 @@ function CourseForm(props) {
         onChange={props.onChange}
         name="title"
         value={props.course.title}
+        error={props.errors.title}
       />
 
       <div className="form-group">
@@ -21,12 +23,16 @@ function CourseForm(props) {
             onChange={props.onChange}
             value={props.course.authorId || ""}
             className="form-control"
+            error={props.errors.authorId}
           >
             <option value="" />
             <option value="1">Cory House</option>
             <option value="2">Scott Allen</option>
           </select>
         </div>
+        {props.errors.authorId && (
+          <div className="alert alert-danger">{props.errors.authorId}</div>
+        )}
       </div>
 
       <TextInput
@@ -35,11 +41,19 @@ function CourseForm(props) {
         onChange={props.onChange}
         name="category"
         value={props.course.category}
+        error={props.errors.category}
       />
 
       <input type="submit" value="Save" className="btn btn-primary" />
     </form>
   );
 }
+
+CourseForm.propTypes = {
+  course: PropTypes.object.isRequired,
+  onSubmit: PropTypes.object.isRequired,
+  onChange: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+};
 
 export default CourseForm;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -4,6 +4,7 @@ import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
 
 function ManageCoursePage(props) {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -22,17 +23,32 @@ function ManageCoursePage(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    //call api
+
+    if (!formIsValid()) return;
+
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course saved.");
     });
   }
 
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) _errors.title = "Title is required.";
+    if (!course.authorId) _errors.authorId = "Author Id is required.";
+    if (!course.category) _errors.category = "Category is required.";
+
+    setErrors(_errors);
+    // Form is valid if errors object has no properties
+    return Object.keys(_errors).length === 0;
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
       <CourseForm
+        errors={errors}
         course={course}
         onChange={handleChange}
         onSubmit={handleSubmit}
